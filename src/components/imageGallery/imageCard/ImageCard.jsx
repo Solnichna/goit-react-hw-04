e4d489b9e7f9c4f855bf738f601c15aef4788c81
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-04-07 14:22:52 +0200

    bigPart

---
+++ src/components/imageGallery/imageCard/ImageCard.jsx
@@ -0,0 +1,9 @@
+const ImageCard = ({ alt, src }) => {
+    return (
+      <div>
+        <img src={src} alt={alt} width="300" height="300" />
+      </div>
+    );
+  };
+
+  export default ImageCard;
