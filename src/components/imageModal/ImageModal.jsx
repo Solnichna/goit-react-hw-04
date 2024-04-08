da71a29227789e6f183b4a2899f6d6ab918b6be8
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-04-08 20:44:13 +0200

    upd

--- src/components/imageModal/ImageModal.jsx
+++ src/components/imageModal/ImageModal.jsx
@@ -1,18 +1,16 @@
 
 import Modal from "react-modal";
 
-const ImageModal = ({ isOpen, onRequestClose, imageUrl }) => {
+const ImageModal = ({ isOpen, onRequestClose, image }) => {
+  if (!isOpen) return null;
+
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Image Modal"
-    >
-      <div className="image-modal-content">
-        <img src={imageUrl} alt="Image" />
+    <div className="modal-overlay" onClick={onRequestClose}>
+      <div className="modal-content">
+        <img src={image.url} alt={image.alt} />
         <button onClick={onRequestClose}>Close</button>
       </div>
-    </Modal>
+    </div>
   );
 };
 
